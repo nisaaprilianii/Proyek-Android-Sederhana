b92e77b128783581fa621ea590fb98c779cd8871
Redmibook <nisaapriliani016@gmail.com>
2023-12-31 08:42:01 +0700

    penambahan button toast

--- app/src/main/java/com/example/myapp/MainActivity.kt
+++ app/src/main/java/com/example/myapp/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.myapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        lateinit var tombol: Button
+
+        tombol = findViewById(R.id.btn)
+
+        tombol.setOnClickListener {
+            Toast.makeText(this, "Selamat Datang di Android", Toast.LENGTH_LONG).show()
+        }
+
     }
 }
